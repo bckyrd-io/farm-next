2dadb71e3dc5befa03b67f8417e100c5dff7a019
bckyrd-io <bckyrd.io@gmail.com>
2024-12-05 06:18:58 +0000

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
+
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async rewrites() {
+        return [
+            {
+                source: '/api/:path*',
+                destination: 'https://next-type-farm.vercel.app/api/:path*', // Replace with your backend API URL
+            },
+        ];
+    },
+};
 
 export default nextConfig;
